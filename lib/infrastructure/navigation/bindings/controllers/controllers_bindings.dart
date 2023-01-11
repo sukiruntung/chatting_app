112be74ba4d26697f88b16d352ca74faaa24e38f
untung sukir <sukiruntung@gmail.com>
2023-01-11 09:08:07 +0700

    styling

---
+++ lib/infrastructure/navigation/bindings/controllers/controllers_bindings.dart
@@ -0,0 +1,1 @@
+export 'package:chatting_app/infrastructure/navigation/bindings/controllers/home.controller.binding.dart'; 
---
+++ lib/presentation/screens.dart
@@ -0,0 +1,1 @@
+export 'package:chatting_app/presentation/home/home.screen.dart';
